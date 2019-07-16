73fc7b90840f913824457942e98e502dae799123
Oladejo <wale@senyours.io>
2019-07-16 12:56:22 -0700

    Activities starting

---
+++ index.js
@@ -0,0 +1,19 @@
+// Activity 1
+
+var pizzaCount = 5
+var totalCost = pizzaCount * 5;
+var name = "Wale"
+var isHungry = true
+
+console.log ("Welcome: " + name);
+console.log ("Pizzas cost $5 each");
+console.log(" your total is: $" + totalCost);
+console.log("Still Hungry: " + isHungry)
+console.log(`This is amazing it is just ${totalCost}` )
+
+
+// Activity 2
+var doYouRock = confirm("The question is,do *you* rock?");
+var howMuchRock = prompt("How much do you rock?")
+
+console.log(doYouRock + howMuchRock)
