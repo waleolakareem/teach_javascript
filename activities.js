c9daa158bcb8a83318bd4e0c80b1f23b0504fc22
Oladejo <wale@senyours.io>
2019-07-17 15:14:02 -0700

    Practice if statements

--- activities.js
+++ activities.js
@@ -16,3 +16,53 @@ if (birthYear <= 1998) {
 else {
   alert("No stake for you")
 }
+
+//Understanding more if statements
+ // if (true === true) {
+ //        alert("hi");
+ //      }
+ //      else {
+ //        alert("bye");
+ //      }
+ //      if (false === false) {
+ //        alert("dude");
+ //      }
+ //      else {
+ //        alert("nope");
+ //      }
+ //      if ("4" === 4) {
+ //        alert("puppies");
+ //      }
+ //      else {
+ //        alert("goats");
+ //      }
+ //      if ("4" == 4) {
+ //        alert("puppies");
+ //      }
+ //      else {
+ //        alert("goats");
+ //      }
+ //      if (true) {
+ //        alert("goo goo dolls");
+ //      }
+ //      else {
+ //        alert("vanilla ice");
+ //      }
+ //      if (false) {
+ //        alert("the neighborhood");
+ //      }
+ //      else {
+ //        alert("one direction");
+ //      }
+ //      if (("5" + "5") === "55") {
+ //        alert("tofu");
+ //      }
+ //      else {
+ //        alert("soybean");
+ //      }
+ //      if ((6 + "5") === "65") {
+ //        alert("richard");
+ //      }
+ //      else {
+ //        alert("branson");
+ //      }
