91d566e0541e74984222951a1e77d96874fc3305
Oladejo <wale@senyours.io>
2019-07-18 23:29:03 -0700

    learn for loop for javascript

---
+++ array.js
@@ -0,0 +1,9 @@
+var drinks = ["Coke", "Pepsi","Water"]
+console.log("First statement");
+
+console.log(drinks[0].toLowerCase());
+
+var vegetables = ["Carrot","Peas","Lettuce","Tomatoes"]
+for (var i = 0; i < vegetables.length; i++) {
+  console.log(`I love ${vegetables[i]}`);
+}
