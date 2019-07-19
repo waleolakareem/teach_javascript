2ac681cb7b327009116f3e08f855c95849b30740
Oladejo <wale@senyours.io>
2019-07-18 23:47:45 -0700

    Use a function to printout backwords

--- array.js
+++ array.js
@@ -7,3 +7,20 @@ var vegetables = ["Carrot","Peas","Lettuce","Tomatoes"]
 for (var i = 0; i < vegetables.length; i++) {
   console.log(`I love ${vegetables[i]}`);
 }
+
+//USe for loop to list
+
+var ZooAnimals = ["Zebra","Rhino","Giraffe","Owl"];
+
+for (var i = 0;i < ZooAnimals.length; i++) {
+  console.log(ZooAnimals[i]);
+}
+
+
+function printout(Things) {
+  for (var i = Things.length - 1; i >= 0; i--) {
+    console.log(Things[i])
+  }
+}
+
+printout("money");
